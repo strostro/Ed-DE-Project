3446505ede13675ee31cb602aa212d49067c8c31
Robin <xie.xiqi@gmail.com>
2025-04-30 06:25:56 +0000

    add new model

---
+++ models/marts/fct_user_event_timeline.sql
@@ -0,0 +1,26 @@
+-- models/marts/fct_user_event_timeline.sql
+
+{{ config(materialized='view') }}
+
+with events as (
+    select
+        user_id,
+        course_id,
+        lesson_id,
+        content_id,
+        event_type,
+        timestamp,
+        video_duration,
+        read_percent,
+        slide_index,
+        question_id,
+        answer_given,
+        is_correct,
+        score,
+        total_questions,
+        completion_ratio
+    from {{ ref('int_user_content_summary') }}
+)
+
+select * from events
+order by user_id, timestamp
