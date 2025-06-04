6d42507c24475d7ca9e08d11aeab22ed3fcbda06
Robin <151415952+strostro@users.noreply.github.com>
2025-06-04 22:43:36 +1200

    Create fct_user_event_timeline.sql

---
+++ dbt-models/marts/fct_user_event_timeline.sql
@@ -0,0 +1,26 @@
+-- models/marts/fct_user_event_timeline.sql
+
+{{ config(materialized='table') }}
+
+with events as (
+    select
+        user_id,
+        course_id,
+        lesson_id,
+        content_id,
+        event_type,
+        timestamp,
+        video_duration,
+        read_percent,
+        slide_index,
+        question_id,
+        answer_given,
+        is_correct,
+        score,
+        total_questions,
+        completion_ratio
+    from {{ ref('int_user_content_events') }}
+)
+
+select * from events
+order by user_id, timestamp
